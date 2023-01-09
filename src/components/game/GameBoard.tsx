e162e544b3b374a1961a9279ad69cb6a499e4a6e
VortexNZ <42858473+gabebnz@users.noreply.github.com>
2023-01-09 21:46:18 +1300

    Beta Release

--- src/components/game/GameBoard.tsx
+++ src/components/game/GameBoard.tsx
@@ -137,7 +137,7 @@ export default function GameBoard({ children }: Props) {
     return (
         <>
         {
-            true && (
+            uno.winner && (
                 <EndScreen />
             )
         }
